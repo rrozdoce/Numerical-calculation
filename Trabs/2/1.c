897cd7d0dcba5c4a33952e32aba6b6c735dd31ef
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-07-10 16:47:03 -0400

    add

--- Trabs/2/1.c
+++ Trabs/2/1.c
@@ -1,48 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
 
-/*
-  Cálculo de determinantes pelo método de Gauss com Pivotação Parcial
-
-  Entrada:
-    -ordem da matriz (Máximo 10)
-    -Matriz
-  Saida:
-   -Resultados de cada passo com 2 casas decimais
-   -Determinante
-   -Tempo de execuçãos
-*/
-
-int main(int argc, char const *argv[])
+int main()
 {
-  time_t start, end;
-  int ordem = 0;
-  float *matriz;
+  int i, j, k, n;
+  double matriz[10][10], c, x[10], soma = 0.0;
+  printf("\nEscreva a ordem da Matriz: ");
+  scanf("%d", &n);
+  printf("\nEscreva os elementos da matriz aumentada: \n\n");
 
-  do
+  for (i = 1; i <= n; i++)
   {
-    printf("Ordem da Matriz:\n");
-    scanf("%d", &ordem);
+    for (j = 1; j <= n; j++)
+    {
+      printf("MATRIZ[%i][%i]: ", i, j);
+      scanf("%lf", &matriz[i][j]);
+    }
+  }
+  for (j = 1; j <= n; j++)
+  {
+    for (i = 1; i <= n; i++)
+    {
+      if (i > j)
+      {
+        c = -(matriz[i][j] / matriz[j][j]);
+        for (k = 1; k <= n + 1; k++)
+        {
+          matriz[i][k] = c * matriz[j][k] + matriz[i][k];
+        }
+      }
+    }
+  }
 
-  } while (ordem > 10);
+  for (i = 1; i <= n; i++)
+  {
+    for (j = 1; j <= n + 1; j++)
+    {
+      if (j != n + 1)
+      {
+        printf("%0.2lf", matriz[i][j]);
+      }
+      else
+      {
+        printf("%0.2lf\n", matriz[i][j]);
+      }
+    }
+    printf("\n");
+  }
 
-  matriz = malloc(ordem * ordem * sizeof(float));
+  x[n] = matriz[n][n + 1] / matriz[n][n];
 
-  // resolucao
-  start = time(NULL);
-  time(&start);
+  for (i = n - 1; i >= 1; i--)
+  {
+    soma = 0;
+    for (j = i + 1; j <= n; j++)
+    {
+      soma = soma + matriz[i][j] * x[j];
+    }
+    x[i] = (matriz[i][n + 1] - soma) / matriz[i][i];
+  }
 
-  //
+  printf("\nA solucao eh: \n");
 
-  free(matriz);
-
-  time(&end);
-
-  double time_taken = (double)end - (double)start;
-
-  printf("Tempo de Execucao: %lfs\n", time_taken);
+  for (i = 1; i <= n; i++)
+  {
+    printf("\nx%d=%lf\t", i, x[i]);
+  }
 
   return 0;
 }
